576c52e69a1661abdc113af0f47c8f132a76ab80
lakrounehamza <lakrounehamza10@gmail.com>
2025-11-11 13:51:18 +0100

    feat(service): add saveDeals method to Deal Service

--- src/main/java/com/progressSoft/fx_deals/service/DealServiceImpl.java
+++ src/main/java/com/progressSoft/fx_deals/service/DealServiceImpl.java
@@ -3,12 +3,16 @@ package com.progressSoft.fx_deals.service;
 import com.progressSoft.fx_deals.dto.DealRequestDto;
 import com.progressSoft.fx_deals.dto.DealResponseDto;
 import com.progressSoft.fx_deals.entity.Deal;
+import com.progressSoft.fx_deals.exception.DealNotFoundException;
 import com.progressSoft.fx_deals.exception.DuplicateDealException;
 import com.progressSoft.fx_deals.exception.InvalidDealDataException;
 import com.progressSoft.fx_deals.mapper.DealMapper;
 import com.progressSoft.fx_deals.repository.DealRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -32,6 +36,23 @@ public class DealServiceImpl implements IDealService {
 
     @Override
     public DealResponseDto getById(String id) {
-        return null;
+        Optional<Deal> deal = dealRepository.findById(id);
+        if(deal.isPresent()) {
+            return dealMapper.toDto(deal.get());
+        }
+        else
+            throw new DealNotFoundException( "Deal not found with ID:"+id);
     }
+
+    @Override
+    public void saveDeals(List<DealRequestDto> dealRequestDtoList) {
+        for (DealRequestDto dto : dealRequestDtoList) {
+            try {
+                save(dto);
+            } catch (Exception e) {
+                System.err.println("Error while importing the deal " + dto.getId() + ": " + e.getMessage());
+            }
+        }
+    }
+
 }
--- src/main/java/com/progressSoft/fx_deals/service/IDealService.java
+++ src/main/java/com/progressSoft/fx_deals/service/IDealService.java
@@ -3,7 +3,10 @@ package com.progressSoft.fx_deals.service;
 import com.progressSoft.fx_deals.dto.DealRequestDto;
 import com.progressSoft.fx_deals.dto.DealResponseDto;
 
+import java.util.List;
+
 public interface IDealService {
     DealResponseDto save(DealRequestDto dealRequestDto);
     DealResponseDto getById(String id);
+    void saveDeals(List<DealRequestDto> dealRequestDtoList);
 }
