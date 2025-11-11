dbf7f154eba95e7bce24b0836e6ae2af16c6b733
lakrounehamza <lakrounehamza10@gmail.com>
2025-11-11 09:10:24 +0100

    Initialize  project

---
+++ src/test/java/com/progressSoft/fx_deals/FxDealsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progressSoft.fx_deals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxDealsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
